4915339c2c31728eb557c9ef23538f5a63bd00d2
Valdir Junior <valdirjcarvalho@outlook.com>
2021-09-13 19:18:39 -0300

    Primeiro Commit

---
+++ index.py
@@ -0,0 +1,10 @@
+# Mystic Dungeon
+# Bem Vindo ao RPG do Grupo 5.
+import random
+
+
+print("\n---------------------------------------------------------------------------")
+nome = str(input("\n Olá jogador, qual o seu nome?\n"))
+print("\n---------------------------------------------------------------------------")
+classe = int(input("\n{}, agora escolha classe\n[1] - Grandioso Mago\n[2] - Honorável Cavaleiro\n[3] - Renomado Arqueiro\n".format(nome)))
+print("\n---------------------------------------------------------------------------")
