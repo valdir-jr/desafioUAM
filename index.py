d4427be786eba33a388a76680780277acf6c65ca
rubedoo <86906153+rubedoo@users.noreply.github.com>
2021-09-13 20:04:04 -0300

    Adicionando as classes

--- index.py
+++ index.py
@@ -8,3 +8,18 @@ nome = str(input("\n Olá jogador, qual o seu nome?\n"))
 print("\n---------------------------------------------------------------------------")
 classe = int(input("\n{}, agora escolha classe\n[1] - Grandioso Mago\n[2] - Honorável Cavaleiro\n[3] - Renomado Arqueiro\n".format(nome)))
 print("\n---------------------------------------------------------------------------")
+
+if (classe == 1):
+    classe = "Grandioso Mago"
+    print("\nEscolheu a classe Grandioso Mago")
+elif(classe == 2):
+    classe = "Honorável Cavaleiro"
+    print("\nEscolheu a classe Honorável Cavaleiro")
+elif(classe == 3):
+    classe = "Renomado Arqueiro"
+    print("\nEscolheu a classe Renomado Arqueiro")
+else:
+    classe = "Pobre"
+    print("\nEscolheu a classe secreta POBRE")
+
+#:ok_hand:
